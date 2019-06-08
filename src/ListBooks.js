203c546318d7b72ece865801a6805726fa54feb0
wangxianke <767774228@qq.com>
2019-06-08 15:32:14 +0800

    set the right shelf value for book on myread page

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -33,12 +33,12 @@ class ListBooks extends Component{
                           <div className="book-top">
                             <div className="book-cover" style={{ width: 128, height: 193, backgroundImage:`url(${book.imageLinks.smallThumbnail})`}}></div>
                             <div className="book-shelf-changer">
-                              <select onChange={(e)=>this.handleChange(book,e)}>
+                              <select defaultValue={book.shelf} onChange={(e)=>this.handleChange(book,e)}>
                                 <option value="move" disabled>Move to...</option>
                                 <option value="currentlyReading">Currently Reading</option>
                                 <option value="wantToRead">Want to Read</option>
                                 <option value="read">Read</option>
-                                <option value="none" selected>None</option>
+                                <option value="none" >None</option>
                               </select>
                             </div>
                           </div>
@@ -60,12 +60,12 @@ class ListBooks extends Component{
                           <div className="book-top">
                             <div className="book-cover" style={{ width: 128, height: 193, backgroundImage:`url(${book.imageLinks.smallThumbnail})`}}></div>
                             <div className="book-shelf-changer">
-                              <select onChange={(e)=>this.handleChange(book,e)}>
+                              <select defaultValue={book.shelf} onChange={(e)=>this.handleChange(book,e)}>
                                 <option value="move" disabled>Move to...</option>
                                 <option value="currentlyReading">Currently Reading</option>
                                 <option value="wantToRead">Want to Read</option>
                                 <option value="read">Read</option>
-                                <option value="none" selected>None</option>
+                                <option value="none" >None</option>
                               </select>
                             </div>
                           </div>
@@ -87,12 +87,12 @@ class ListBooks extends Component{
                           <div className="book-top">
                             <div className="book-cover" style={{ width: 128, height: 193, backgroundImage:`url(${book.imageLinks.smallThumbnail})`}}></div>
                             <div className="book-shelf-changer">
-                              <select onChange={(e)=>this.handleChange(book,e)}>
+                              <select defaultValue={book.shelf} onChange={(e)=>this.handleChange(book,e)}>
                                 <option value="move" disabled>Move to...</option>
                                 <option value="currentlyReading">Currently Reading</option>
                                 <option value="wantToRead">Want to Read</option>
                                 <option value="read">Read</option>
-                                <option value="none" selected>None</option>
+                                <option value="none" >None</option>
                               </select>
                             </div>
                           </div>
